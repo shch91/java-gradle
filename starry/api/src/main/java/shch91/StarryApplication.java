46714b6393bd0e4548bbfaab7a0fb78c3f4a800e
shch91 <github2016>
2019-05-21 12:31:03 +0800

    fghjkl;

--- starry/api/src/main/java/shch91/StarryApplication.java
+++ starry/api/src/main/java/shch91/StarryApplication.java
@@ -1,6 +1,10 @@
 package shch91;
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +15,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+
 @EnableAsync
 @EnableCaching
 @EnableScheduling
 @SpringBootApplication
-@ImportResource(locations={"classpath:spring/spring.xml"})
+@ImportResource(locations = {"classpath:spring/spring.xml"})
 @Import(value = {ServiceApplication.class})
 public class StarryApplication {
 
@@ -30,5 +36,14 @@ public class StarryApplication {
     }
 
 
+    @Bean
+    @Qualifier(value = "zk")
+    public ZooKeeper createZookeeper(@Value("${zookeeper.server}") String servers) throws IOException {
+        return new ZooKeeper(servers, 5000, new Watcher() {
+            // 监控所有被触发的事件
+            public void process(WatchedEvent event) {
+            }
+        });
+    }
 
 }
---
+++ starry/service/src/main/java/shch91/zk/ZkMaster.java
@@ -0,0 +1,67 @@
+package shch91.zk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import static org.apache.zookeeper.ZooDefs.Ids.OPEN_ACL_UNSAFE;
+
+@Slf4j
+@Component
+public class ZkMaster {
+
+    private static final String MASTER = "/master";
+
+    private static final String serverId = "1";
+
+    private boolean isLeader = false;
+
+    @Autowired
+    @Qualifier(value = "zk")
+    private  ZooKeeper zk;
+
+    private boolean checkMaster()throws InterruptedException{
+        while (true) {
+            try {
+                Stat stat = new Stat();
+
+                byte[] data = zk.getData(MASTER, false, stat);
+                isLeader = new String(data).equals(serverId);
+                return true;
+            }catch (KeeperException.NoNodeException e){
+                return  false;
+            }catch (KeeperException.ConnectionLossException e){
+                log.info("[ZkMaster.checkMaster] connection lossed");
+            }  catch (KeeperException e) {
+                log.info("[ZkMaster.KeeperException] error");
+            }
+        }
+    }
+
+    private void  runForMaster() throws InterruptedException{
+        while (true){
+            try{
+                zk.create(MASTER,serverId.getBytes(),OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+                isLeader=true;
+                break;
+            }catch (KeeperException.NodeExistsException e){
+                isLeader=false;
+                break;
+            }catch (KeeperException.ConnectionLossException e){
+                log.info("[ZkMaster.runForMaster] connection lossed");
+            } catch (KeeperException e) {
+                log.info("[ZkMaster.KeeperException] error");
+            }
+            if(checkMaster()){
+                break;
+            }
+        }
+    }
+
+
+}
